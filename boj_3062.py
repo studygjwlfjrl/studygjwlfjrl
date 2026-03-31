097067a8245377d10dc7388f445f2edb1bfe8dc5
studygjwlfjrl <heojiheok@gmail.com>
2026-03-31 14:06:58 +0900

    #3062

---
+++ boj_3062.py
@@ -0,0 +1,10 @@
+import random
+rand_num = random.randrange(10,100001)
+num = str(rand_num)
+num_rev = int(num[::-1])
+num1 = int(num)
+num2 = num1 + num_rev
+num2_rev = int(str(num2)[::-1])
+if num2 == num2_rev:
+    print("Yes")
+else:    print("No")
