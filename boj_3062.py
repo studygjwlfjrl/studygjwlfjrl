df6c82fa15f30f391a31ac1d5d83714bcd319393
studygjwlfjrl <heojiheok@gmail.com>
2026-04-01 13:49:14 +0900

    correct yarr

--- boj_3062.py
+++ boj_3062.py
@@ -1,10 +1,23 @@
-import random
-rand_num = random.randrange(10,100001)
-num = str(rand_num)
-num_rev = int(num[::-1])
-num1 = int(num)
-num2 = num1 + num_rev
-num2_rev = int(str(num2)[::-1])
-if num2 == num2_rev:
-    print("Yes")
-else:    print("No")
+# import random
+# rep = int(input())
+# for _ in range(rep):
+#  rand_num = random.randrange(10,100001)
+#  num = str(rand_num)
+#  num_rev = int(num[::-1])
+#  num1 = int(num)
+#  num2 = num1 + num_rev
+#  num2_rev = int(str(num2)[::-1])
+#  if num2 == num2_rev:
+#      print("Yes")
+#  else:    print("No")
+
+rep = int(input())
+for _ in range(rep):
+    num = input()
+    num_rev = int(num[::-1])
+    num1 = int(num)
+    num2 = num1 + num_rev
+    num2_rev = int(str(num2)[::-1])
+    if num2 == num2_rev:
+        print("YES")
+    else:    print("NO")
